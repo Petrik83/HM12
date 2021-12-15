63176928e48e516d148d02fa789f8f16fec058d9
Aliaksandr Piatrovich <alex.petrovich83@gmail.com>
2021-12-15 17:41:55 +0300

    Created switch logic.

--- HM12/ViewController.swift
+++ HM12/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController {
                 2: ["Уведомления", "Звуки, тактильные сигналы", "Фокусирование", "Экранное время"],
                 3: ["Основные", "Пункт управления", "Экран и яркость", "Экран 'Домой'"]]
     var defaultDetailData = [0: ["Apple ID, iCloud, контент и покупки"],
-                        1: ["", "Выкл.", "Вкл.", "", "", "Не подключено"]]
+                             1: ["", "Выкл.", "Вкл.", "", "", "Не подключено"]]
     
     lazy var tableView: UITableView = {
         let tableView = UITableView(frame: .zero, style: .insetGrouped)
@@ -23,8 +23,8 @@ class ViewController: UIViewController {
         tableView.dataSource = self
         tableView.delegate = self
         return tableView
-        }()
-
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Настройки"
@@ -36,9 +36,9 @@ class ViewController: UIViewController {
         search.searchResultsUpdater = self
         self.navigationItem.searchController = search
         
-
+        
     }
-
+    
     private func viewHierarchy() {
         view.addSubview(tableView)
     }
@@ -51,7 +51,7 @@ class ViewController: UIViewController {
         tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
         
     }
-
+    
     class TableViewSell: UITableViewCell {
         override var reuseIdentifier: String? {
             return idCell
@@ -60,55 +60,69 @@ class ViewController: UIViewController {
 }
 
 extension ViewController: UITableViewDataSource, UITableViewDelegate {
-        func numberOfSections(in tableView: UITableView) -> Int {
-            return data.count
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return data.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data[section]?.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCell(withIdentifier: idCell)
+        if cell == nil {
+            cell = UITableViewCell(style: .default, reuseIdentifier: idCell)
         }
         
-        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-            return data[section]?.count ?? 0
-        }
-        
-        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-            var cell = tableView.dequeueReusableCell(withIdentifier: idCell)
-            if cell == nil {
+        if indexPath.section == 0 {
+            cell = UITableViewCell(style: .subtitle, reuseIdentifier: idCell)
+            cell?.accessoryType = .disclosureIndicator
+        } else {
+            if indexPath.section == 1 && indexPath.row == 0 {
                 cell = UITableViewCell(style: .default, reuseIdentifier: idCell)
-            }
-
-            if indexPath.section == 0 {
-                cell = UITableViewCell(style: .subtitle, reuseIdentifier: idCell)
-                cell?.accessoryType = .disclosureIndicator
+                let aviaModeSwitch = UISwitch()
+                aviaModeSwitch.addTarget(self, action: #selector(self.switchChanged(_:)), for: .valueChanged)
+                aviaModeSwitch.setOn(false, animated: true)
+                cell!.accessoryView = aviaModeSwitch
+                cell?.selectionStyle = .none
             } else {
-                if indexPath.section == 1 && indexPath.row == 0 {
-                    cell = UITableViewCell(style: .default, reuseIdentifier: idCell)
-                    let aviaModeSwitch = UISwitch()
-                    aviaModeSwitch.addTarget(self, action: #selector(self.switchChanged(_:)), for: .valueChanged)
-                    aviaModeSwitch.setOn(false, animated: true)
-                    cell!.accessoryView = aviaModeSwitch
-                    cell?.selectionStyle = .none
+                if indexPath.section == 1 && (indexPath.row != 0) {
+                    cell = UITableViewCell(style: .value1, reuseIdentifier: idCell)
+                    cell?.accessoryType = .disclosureIndicator
                 } else {
-                    if indexPath.section == 1 && (indexPath.row != 0) {
-                        cell = UITableViewCell(style: .value1, reuseIdentifier: idCell)
-                        cell?.accessoryType = .disclosureIndicator
-                    } else {
-                        cell?.accessoryType = .disclosureIndicator
-                    }
+                    cell?.accessoryType = .disclosureIndicator
                 }
             }
-            let image = self.data[indexPath.section]?[indexPath.row]
-                
-            
-            cell?.imageView?.image = UIImage(named: image ?? "")
-                                             
-            cell?.textLabel?.text = self.data[indexPath.section]?[indexPath.row]
-            cell?.detailTextLabel?.text = defaultDetailData[indexPath.section]?[indexPath.row]
-            
-         
-            return cell!
+        }
+        let image = self.data[indexPath.section]?[indexPath.row]
+        
+        
+        cell?.imageView?.image = UIImage(named: image ?? "")
+        
+        cell?.textLabel?.text = self.data[indexPath.section]?[indexPath.row]
+        cell?.detailTextLabel?.text = defaultDetailData[indexPath.section]?[indexPath.row]
+        
+        
+        return cell!
+        
+    }
+    @objc func switchChanged(_ sender : UISwitch!){
+        if sender.isOn {
+            tableView.cellForRow(at: IndexPath(row: 1, section: 1))?.detailTextLabel?.text = "Выкл."
+            tableView.cellForRow(at: IndexPath(row: 2, section: 1))?.detailTextLabel?.text = "Выкл."
+            tableView.cellForRow(at: IndexPath(row: 3, section: 1))?.detailTextLabel?.text = "Авиарежим"
+            tableView.cellForRow(at: IndexPath(row: 4, section: 1))?.detailTextLabel?.text = "Выкл."
+            tableView.cellForRow(at: IndexPath(row: 5, section: 1))?.detailTextLabel?.text = "Не подключено"
+        } else {
+            //заменить на предыдущие значения
+            tableView.cellForRow(at: IndexPath(row: 1, section: 1))?.detailTextLabel?.text = defaultDetailData[1]?[1]
+            tableView.cellForRow(at: IndexPath(row: 2, section: 1))?.detailTextLabel?.text = defaultDetailData[1]?[2]
+            tableView.cellForRow(at: IndexPath(row: 3, section: 1))?.detailTextLabel?.text = defaultDetailData[1]?[3]
+            tableView.cellForRow(at: IndexPath(row: 4, section: 1))?.detailTextLabel?.text = defaultDetailData[1]?[4]
+            tableView.cellForRow(at: IndexPath(row: 5, section: 1))?.detailTextLabel?.text =  defaultDetailData[1]?[5]
             
         }
-    @objc func switchChanged(_ sender : UISwitch!){
-        print("switchChange")
-            }
+    }
 }
 
 extension ViewController: UISearchResultsUpdating {
