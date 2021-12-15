5aee24ae049083883483d2793fa25af728b6c958
Aliaksandr Piatrovich <alex.petrovich83@gmail.com>
2021-12-15 16:58:38 +0300

    Add tablleView

--- HM12/ViewController.swift
+++ HM12/ViewController.swift
@@ -6,14 +6,70 @@
 //
 
 import UIKit
-
+let idCell = "TableViewSell"
 class ViewController: UIViewController {
+    
+    
+    var data = [0: ["Александр Петрович"],
+                1: ["Авиарежим", "Wi-Fi", "Bluetooth", "Сотовая связь", "Режим модема", "VPN"],
+                2: ["Уведомления", "Звуки, тактильные сигналы", "Фокусирование", "экранное время"],
+                3: ["Основные", "Пункт управления", "Экран и яркость", "Экран 'Домой'"]]
+    
+    lazy var tableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .insetGrouped)
+        tableView.backgroundColor = .systemGray6
+        tableView.dataSource = self
+        tableView.delegate = self
+        return tableView
+        }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        viewHierarchy()
+        setupLayout()
+
     }
 
+    private func viewHierarchy() {
+        view.addSubview(tableView)
+    }
+    
+    private func setupLayout() {
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+        tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+        
+    }
 
+    class TableViewSell: UITableViewCell {
+        override var reuseIdentifier: String? {
+            return idCell
+        }
+    }
+}
+
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+        func numberOfSections(in tableView: UITableView) -> Int {
+            return data.count
+        }
+        
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return data[section]?.count ?? 0
+        }
+        
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            var cell = tableView.dequeueReusableCell(withIdentifier: idCell)
+            if cell == nil {
+                cell = UITableViewCell(style: .default, reuseIdentifier: idCell)
+            }
+
+            cell?.textLabel?.text = self.data[indexPath.section]?[indexPath.row]
+            
+         
+            return cell!
+            
+        }
 }
 
